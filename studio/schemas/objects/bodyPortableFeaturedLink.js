a2f87f08aece4380d0d51e37ec297db07aa47162
Tatsumi Suzuki <djtatsumisuzuki@gmail.com>
2021-01-27 21:22:32 +0100

    Refactor portable text parts.

--- studio/schemas/objects/bodyPortableFeaturedLink.js
+++ studio/schemas/objects/bodyPortableFeaturedLink.js
@@ -4,10 +4,21 @@ export default {
   title: 'Länk',
   fields: [
     {
+      title: 'Namn',
+      name: 'linkName',
+      type: 'string',
+    },
+    {
+      title: 'PDF',
+      name: 'isPdf',
+      type: 'boolean',
+      description: 'Markera om länken är en PDF.'
+    },
+    {
       title: 'Länk',
       name: 'href',
       type: 'url',
       allowRelative: true,
-    }
+    },
   ]
 }
