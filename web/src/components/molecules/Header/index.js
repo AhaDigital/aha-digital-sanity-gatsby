ff1bbc4f7fbf47a3b6b9ade79f1c373344a6156a
Tatsumi Suzuki <djtatsumisuzuki@gmail.com>
2021-03-09 13:26:38 +0100

    Change svg folder name

--- web/src/components/molecules/Header/index.js
+++ web/src/components/molecules/Header/index.js
@@ -2,7 +2,7 @@ import React, { createRef, useEffect, useState } from 'react'
 import get from 'lodash.get'
 import { useBreakpoint } from 'gatsby-plugin-breakpoints';
 import Icon from '../../atoms/icon'
-import AhaDigitalLogo from '../../atoms/Svg/AhaDigitalLogo'
+import AhaDigitalLogo from '../../atoms/svg/ahaDigitalLogo'
 import Button from '../../atoms/Button'
 import LinkButton from '../../atoms/LinkButton'
 import TransparentButton from '../../atoms/TransparentButton'
