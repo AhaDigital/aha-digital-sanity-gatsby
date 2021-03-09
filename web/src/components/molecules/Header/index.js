67bc0eed213900ccd63fceea82d3913f2e255fa8
Tatsumi Suzuki <djtatsumisuzuki@gmail.com>
2021-03-09 19:41:35 +0100

    Remove logo

--- web/src/components/molecules/Header/index.js
+++ web/src/components/molecules/Header/index.js
@@ -2,7 +2,6 @@ import React, { createRef, useEffect, useState } from 'react'
 import get from 'lodash.get'
 import { useBreakpoint } from 'gatsby-plugin-breakpoints';
 import Icon from '../../atoms/icon'
-import AhaDigitalLogo from '../../atoms/svg/ahaDigitalLogo'
 import Button from '../../atoms/Button'
 import LinkButton from '../../atoms/LinkButton'
 import TransparentButton from '../../atoms/TransparentButton'
@@ -135,7 +134,6 @@ const Header = ({
             addSpeach: addSpeach
           }}
         >
-          <AhaDigitalLogo isAnimated={foldHeader} />
         </NavLevel.Link>
         <NavLevel.MobileMenuButton
           name="menuButton"
