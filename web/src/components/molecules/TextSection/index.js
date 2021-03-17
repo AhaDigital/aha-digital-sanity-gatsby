b38a8fca04499a006ef47c0b9dec24d0b4972fbf
Tatsumi Suzuki <djtatsumisuzuki@gmail.com>
2021-03-17 20:49:23 +0100

    Add contrast to text block

--- web/src/components/molecules/TextSection/index.js
+++ web/src/components/molecules/TextSection/index.js
@@ -2,7 +2,14 @@ import React from 'react'
 import Grid from '../Grid'
 import PortableText from '../../atoms/portableText'
 
-const TextSection = ({ bodyPortableText, BlockDirectionLeft }) => {
+const TextSection = ({ bodyPortableText, BlockDirectionLeft, addContrast }) => {
+
+  bodyPortableText.forEach(element => {
+    const {style} = element
+    if(style === 'h2' || style === 'h3') {
+      element.addContrast = addContrast
+    }
+  })
   return (
     <Grid tagName="section" maxWidth="default" withPadding marginTop="xxl" justify={BlockDirectionLeft ? 'flex-start' : 'center'}>
       <Grid.Unit withGutter size={{sm: 12, md: 10, lg: 7}}>
