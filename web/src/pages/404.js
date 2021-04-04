9fb7796a5c3ca3adf62a65393e298019177a3416
Tatsumi Suzuki <djtatsumisuzuki@gmail.com>
2021-04-04 23:21:54 +0200

    Fix broken 404

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import App from '../app'
 
-const pageSEO = { ogTitle: '404: Not found', ogDescription: 'hej'}
+const pageSEO = { ogTitle: '404: Not found'}
 
-const NotFoundPage = () => (
-  <App pageSEO={pageSEO}>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </App>
-)
+const NotFoundPage = (props) => {
+  const {location} = props
+  return (
+
+    <App pageSEO={pageSEO} location={location}>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </App>
+  )
+}
 
 export default NotFoundPage
