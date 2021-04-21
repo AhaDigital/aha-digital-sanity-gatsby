2a10c0c6d03bbdf680502c57d7fc88ac39ae9e76
Tatsumi Suzuki <hej@ahadigital.se>
2021-04-21 22:13:07 +0200

    Remove comments.

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,9 +1,3 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
 async function createLandingPages (graphql, actions) {
   const {createPage} = actions
   const result = await graphql(`
